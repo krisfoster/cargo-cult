ee8553f3bf21120588b5d8eb294c627a43f25b6c
krisfoster <me@krismade.me>
2022-12-05 11:09:03 +0000

    Add install for docker-compose

--- .installs/install-ol8-docker-k8s.sh
+++ .installs/install-ol8-docker-k8s.sh
@@ -87,6 +87,10 @@ sudo groupadd -f docker
 sudo usermod -aG docker ${USER}
 sudo usermod -a -G docker $USER
 
+# Install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Python oci
 #sudo dnf -y install python36-oci-cli
 
